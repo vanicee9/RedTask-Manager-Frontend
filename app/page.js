2c4633411cc5cc309469e838cc3bb1aa4b80201b
manish-sharma-reddoorz <manish.sharma@reddoorz.com>
2024-03-12 01:15:09 +0530

    Converted TaskForm from tailwindcss to scss and also made form dark

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-// import TaskForm from "./components/TaskForm";
+import TaskForm from "./components/TaskForm/TaskForm";
 import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
 export default function Home() {
@@ -10,9 +10,8 @@ export default function Home() {
       alignItems: 'center',
       justifyContent: 'center'
     }}>
-      {/* <TaskForm /> */}
-      <LoginForm/>
-      <SignUpForm/>
+      <TaskForm />
+     
     </div>
   );
 }     
