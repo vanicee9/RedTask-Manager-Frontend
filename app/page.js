67e9a05bfde56ba4fe6758926c269167a0867bfa
manish-sharma-reddoorz <manish.sharma@reddoorz.com>
2024-03-14 18:23:55 +0530

    Modified Task Form Style

--- app/page.js
+++ app/page.js
@@ -3,15 +3,8 @@ import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
 export default function Home() {
   return (
-    <div className="full-screen"style = {{
-      height: '100vh',
-      width: '100vw',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
+    <div >
       <TaskForm />
-     
     </div>
   );
 }     
