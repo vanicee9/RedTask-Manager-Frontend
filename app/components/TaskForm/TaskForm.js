a2964fe28d1c000e1b6ccc1dafb0060dfcc0c2be
manish-sharma-reddoorz <manish.sharma@reddoorz.com>
2024-03-20 10:26:49 +0530

    user email based autosuggestion for assign to

--- app/components/TaskForm/TaskForm.js
+++ app/components/TaskForm/TaskForm.js
@@ -1,86 +1,84 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import './TaskForm.scss';
-import Spinner from '../Spinner';
-import useDebounceSearch from '@/app/utils/useDebouncedSearch';
-const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+"use client";
+import React, { useEffect, useState } from "react";
+import "./TaskForm.scss";
+import Spinner from "../Spinner";
+import useDebounceSearch from "@/app/utils/useDebouncedSearch";
+const EMAIL_REGEX =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const VERSION = process.env.NEXT_PUBLIC_VERSION;
-export default function TaskForm({taskCard}) {
-
+export default function TaskForm({ taskCard }) {
   const [formData, setFormData] = useState({
-    id: '',
-    title: '',
-    description: '',
-    deadline: '',
-    phase: 'New',
-    assignTo: '',
-    assignFrom: '',
+    id: "",
+    title: "",
+    description: "",
+    deadline: "",
+    phase: "New",
+    assignTo: "",
+    assignFrom: "",
   });
   const [errorObject, setErrorObject] = useState({
-    title_error: '',
-    description_error: '',
-    deadline_error: '',
-    phase_error: '',
-    assignTo_error: '',
-  }) 
+    title_error: "",
+    description_error: "",
+    deadline_error: "",
+    phase_error: "",
+    assignTo_error: "",
+  });
   const [loading, setLoading] = useState(true);
 
-
+  const [userEmails, setUserEmails] = useState([]);
   const debouncedSearchValue = useDebounceSearch(formData.assignTo);
   useEffect(() => {
     fetch(`${BASE_URL}${VERSION}/users/findByEmail/${debouncedSearchValue}`)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((error) => {
-      console.log("Something went wrong while fetching");
-      console.log(error);
-    })
-  },[debouncedSearchValue])
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setUserEmails(response.data)
+      })
+      .catch((error) => {
+        console.log("Something went wrong while fetching");
+        console.log(error);
+      });
+  }, [debouncedSearchValue]);
 
   useEffect(() => {
-    if(Object.keys(taskCard).length > 0) setFormData(taskCard)
-  },[taskCard])
+    if (Object.keys(taskCard).length > 0) setFormData(taskCard);
+  }, [taskCard]);
 
-
-  function handleValidation(event){
-    const {name, value} = event.target;
+  function handleValidation(event) {
+    const { name, value } = event.target;
     let ok = true;
-    if(['title', 'description'].includes(name)) {
-      if(!value){
+    if (["title", "description"].includes(name)) {
+      if (!value) {
         setErrorObject((prev) => ({
           ...prev,
-          [`${name}_error`]: `${name} required`
-        }))
+          [`${name}_error`]: `${name} required`,
+        }));
+        ok = false;
+      }
+    } else if (name === "deadline") {
+      if (!value || value < new Date()) {
+        setErrorObject((prev) => ({
+          ...prev,
+          [`${name}_error`]: "Present or future date required",
+        }));
+        ok = false;
+      }
+    } else if (name === "assignTo") {
+      if (!value || !value.match(EMAIL_REGEX)) {
+        setErrorObject((prev) => ({
+          ...prev,
+          [`${name}_error`]: "email not valid",
+        }));
         ok = false;
       }
     }
-    else if(name === 'deadline') {
-      if(!value || value < new Date()) {
-        setErrorObject((prev) => ({
-          ...prev,
-          [`${name}_error`]: 'Present or future date required'
-        }))
-        ok = false;
-      }
-    }
-    else if (name === 'assignTo') {
-      if(!value || !value.match(EMAIL_REGEX)) {
-        setErrorObject((prev) => ({
-          ...prev,
-          [`${name}_error`]: 'email not valid'
-        }))
-        ok = false;
-      }
-    }
-    
 
-
-    if(ok){
+    if (ok) {
       setErrorObject((prev) => ({
         ...prev,
-        [`${name}_error`]: ''
-      }))
+        [`${name}_error`]: "",
+      }));
     }
 
     return ok;
@@ -90,7 +88,7 @@ export default function TaskForm({taskCard}) {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -100,86 +98,128 @@ export default function TaskForm({taskCard}) {
   };
 
   return (
-      <div className="blur-background">
-            <div className="task-form">
-              <h2>Task Information</h2>
-              <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label  htmlFor="title">Title</label>
-                  <input 
-                    type="text" 
-                    id="title" 
-                    name="title" 
-                    value={formData.title}
-                    onChange={handleChange} 
-                    placeholder="New Task..."
-                    onBlur={handleValidation}
-                  />
-                  <div className="error-container" name="title_error" id="title-error">{errorObject.title_error}</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="description">Description</label>
-                  <textarea 
-                    id="description" 
-                    name="description"
-                    value={formData.description}
-                    onChange={handleChange}
-                    placeholder="Task description..."
-                    onBlur={handleValidation}
-
-                  ></textarea>
-                  <div className="error-container" name="description_error" id="description-error">{errorObject.description_error}</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="deadline">Deadline Date</label>
-                  <input 
-                    type="date" 
-                    id="deadline" 
-                    name="deadline" 
-                    value={formData.deadline}
-                    onChange={handleChange} 
-                    onBlur={handleValidation}
-                  />
-                  <div className="error-container" name="deadline_error" id="deadline-error">{errorObject.deadline_error}</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="phase">Phase</label>
-                  <select 
-                    id="phase" 
-                    name="phase" 
-                    value={formData.phase}
-                    onChange={handleChange}
-                    onBlur={handleValidation}
-                  >
-                    <option value="New">New</option>
-                    <option value="UI/UX">UI/UX</option>
-                    <option value="Development">Development</option>
-                    <option value="Testing">Testing</option>
-                    <option value="UAT">UAT</option>
-                    <option value="Released">Released</option>
-                  </select>
-                  <div className="error-container" name="phase_error" id="phase-error">{errorObject.phase_error}</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="assignTo">Assign To</label>
-                  <input 
-                    type="text" 
-                    id="assignTo" 
-                    name="assignTo" 
-                    value={formData.assignTo}
-                    onChange={handleChange} 
-                    placeholder="johndoe@gmail.com"
-                    onBlur={handleValidation}
-                  />
-                  <div className="error-container" name="assignTo_error" id="assignTo-error">{errorObject.assignTo_error}</div>
-                </div>
-                <button type="submit">
-                  {
-                    loading ? <Spinner color={"#fff"} height={"25"} width={"25"}/> : "Submit"
-                  }
-                </button>
-              </form>
+    <div className="blur-background">
+      <div className="task-form">
+        <h2>Task Information</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              placeholder="New Task..."
+              onBlur={handleValidation}
+            />
+            <div
+              className="error-container"
+              name="title_error"
+              id="title-error"
+            >
+              {errorObject.title_error}
             </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Task description..."
+              onBlur={handleValidation}
+            ></textarea>
+            <div
+              className="error-container"
+              name="description_error"
+              id="description-error"
+            >
+              {errorObject.description_error}
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="deadline">Deadline Date</label>
+            <input
+              type="date"
+              id="deadline"
+              name="deadline"
+              value={formData.deadline}
+              onChange={handleChange}
+              onBlur={handleValidation}
+            />
+            <div
+              className="error-container"
+              name="deadline_error"
+              id="deadline-error"
+            >
+              {errorObject.deadline_error}
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="phase">Phase</label>
+            <select
+              id="phase"
+              name="phase"
+              value={formData.phase}
+              onChange={handleChange}
+              onBlur={handleValidation}
+            >
+              <option value="New">New</option>
+              <option value="UI/UX">UI/UX</option>
+              <option value="Development">Development</option>
+              <option value="Testing">Testing</option>
+              <option value="UAT">UAT</option>
+              <option value="Released">Released</option>
+            </select>
+            <div
+              className="error-container"
+              name="phase_error"
+              id="phase-error"
+            >
+              {errorObject.phase_error}
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="assignTo">Assign To</label>
+            <input
+              type="text"
+              id="assignTo"
+              name="assignTo"
+              value={formData.assignTo}
+              onChange={handleChange}
+              placeholder="johndoe@gmail.com"
+              onBlur={handleValidation}
+              list={'suggestion'}
+              autoComplete="false"
+            />
+            <datalist id="suggestion">
+              {userEmails.length > 0 && userEmails.map((user, index) => {
+                return (
+                  <option key={index} value={user.email}>
+                    {user.name}
+                  </option>
+                );
+              })}
+            </datalist>
+            <div
+              className="error-container"
+              name="assignTo_error"
+              id="assignTo-error"
+            >
+              {errorObject.assignTo_error}
+            </div>
+          </div>
+          <button type="submit">
+            {loading ? (
+              <Spinner color={"#fff"} height={"25"} width={"25"} />
+            ) : (
+              "Submit"
+            )}
+          </button>
+        </form>
       </div>
+    </div>
   );
 }
