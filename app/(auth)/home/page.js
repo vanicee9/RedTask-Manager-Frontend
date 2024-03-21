377198aec6635a3f7badce861ba412951ae14d7b
Harsh Jain <harsh.jain@reddoorz.com>
2024-03-21 18:02:20 +0530

    added home page and redirect method in main page

--- app/(auth)/home/page.js
+++ app/(auth)/home/page.js
@@ -1,7 +1,8 @@
+import HomePage from '@/app/components/Home/HomePage'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <HomePage />
   )
 }
---
+++ app/components/Home/TaskContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskContainer = () => {
+  return (
+    <div>TaskContainer</div>
+  )
+}
+
+export default TaskContainer
