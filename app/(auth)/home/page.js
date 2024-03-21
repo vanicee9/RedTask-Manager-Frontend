ff737367100f8940dc17ef35a29ea3d707a3c1c5
Harsh Jain <harsh.jain@reddoorz.com>
2024-03-21 09:59:06 +0530

    home auth created

--- app/(auth)/home/page.js
+++ app/(auth)/home/page.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import SignUp from '@/app/components/signUp/SignUp'
 
 export default function page() {
   return (
-    <SignUp/>
+    <div>page</div>
   )
 }
--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,14 +1,8 @@
 import LoginForm from '@/app/components/login/LoginForm'
-// import Head from 'next/head'
 import React from 'react'
-
-
 
 export default function page() {
   return (
-    
-    <> <LoginForm/></>
-     
-   
+    <LoginForm/>
   )
 }
